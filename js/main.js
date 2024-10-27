7d207dbd5430bbf2516353b8f78fab598e01c61c
Aviv Gibli <avivgib1012@gmail.com>
2024-10-27 11:26:19 +0200

    Increase ball by random size

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 'use strict'
 
 function onBallClick(elBall) {
-    const newSize = elBall.clientWidth >= 400 ? 100 : elBall.clientWidth + 50  
+    const randomIncrementSize = getRandomInt(20, 61)
+    const newSize = (elBall.clientWidth + randomIncrementSize) >= 400 ? 100 : (elBall.clientWidth + randomIncrementSize)  
     elBall.style.width = elBall.style.height = `${newSize}px`
     elBall.innerHTML = newSize
 }
