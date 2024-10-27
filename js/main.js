38ba48952f843da78884fc512e100186aea0e1e4
Aviv Gibli <avivgib1012@gmail.com>
2024-10-27 04:47:08 +0200

    Limit the ball size

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 'use strict'
 
 function onBallClick(elBall) {
-    let currentWidth = elBall.clientWidth
-    let newWidth = currentWidth + 50
-
-    elBall.style.width = newWidth + 'px'
-    elBall.style.height = newWidth + 'px'
-    elBall.innerHTML = newWidth
+    const newSize = elBall.clientWidth >= 400 ? 100 : elBall.clientWidth + 50  
+    elBall.style.width = elBall.style.height = `${newSize}px`
+    elBall.innerHTML = newSize
 }
